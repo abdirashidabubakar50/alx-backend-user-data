594b4ba84e97afab44c661ced1d3b4a880f181b9
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-12 23:54:19 +0300

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ A class BasicAuth that inherits from the class Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,22 @@ class BasicAuth(Auth):
         if authorization_header and authorization_header.startswith("Basic "):
             return authorization_header[len("Basic "):]
         return None
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ decodes teh authorization header file as UTF-8 string
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(
+                base64_authorization_header, validate=True
+            )
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, ValueError):
+            return None
