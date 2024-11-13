010d443f1117b868dcffa59f272f612d6d3b7e88
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-13 13:58:37 +0300

    Basic - User Object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,6 +65,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
     ) -> TypeVar('User'):
+        """ Returns User instance based on email and password
+        """
         if user_email is None or not isinstance(user_email, str):
             return None
 
