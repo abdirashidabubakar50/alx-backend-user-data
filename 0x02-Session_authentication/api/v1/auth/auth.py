52a7b3049bd64c18633dc924f51257595f7f87f6
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-14 11:35:35 +0300

    Session cookies

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -7,7 +7,6 @@ from flask import request
 from typing import List, TypeVar
 
 
-
 class Auth:
     """ Manages API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
