88d755625a0da98dd23a061817b9298a3783c7ad
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-18 14:56:14 +0300

    Created the User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+""" This model defines a model named User
+"""
+Base = declarative_base()
+
+
+class User(Base):
+    """ Model User, defines database table users
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer,  primary_key=True, autoincrement=True)
+    name = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=True)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
