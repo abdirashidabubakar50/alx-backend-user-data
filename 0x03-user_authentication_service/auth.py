70a8be924cd5b11a4eaa2338147710514291e707
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-20 12:46:49 +0300

    find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,6 +73,19 @@ class Auth:
             return None
 
 
+    def get_user_from_session_id(session_id: str) -> User:
+        """ Retrieve a user by their session id
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+
 def _hash_password(password: str) -> bytes:
 
     """ Hashes a password with bcrypt using a generated salt.
