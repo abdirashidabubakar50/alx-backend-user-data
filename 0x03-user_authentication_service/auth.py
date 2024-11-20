10118d6c961e845cea16a8d84dc929d7e8b18d8f
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-20 14:31:40 +0300

    generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,6 +93,20 @@ class Auth:
         except NoResultFound:
             pass
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ updates the user's reset_token database field
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError("user does not exist")
+
+        reset_token = self._generate_uuid()
+
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
+
 
 def _hash_password(password: str) -> bytes:
 
