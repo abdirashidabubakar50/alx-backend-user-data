44bc52817ec33fc6d3716e43c04d562b3848e4fb
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-11-22 22:48:17 +0300

    regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,13 +7,13 @@ returns log message obfuscated
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
+                 separator: str) -> str:
     """
     Returns the log message wiath specified fields obfuscated
 
     Args:
         fields (List[str]):  Fields to obfuscate
-        redaction (str): The string to prelace the field values with.
+        redaction (str): The string to replace the field values with.
         message (str): The log message
         separator (str): The field separator in the log message
 
@@ -21,5 +21,5 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         str: The obfuscated log message
     """
     pattern = (f"({'|'.join(re.escape(field) for field in fields)})=.*?"
-               f"(?={re.escape(seperator)}|$)")
+               f"(?={re.escape(separator)}|$)")
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
